744b84d41fe12f3da2bee2ba49c0d03feb894f02
samuel Kabbaj <kabbaj.samuel@gmail.com>
2018-10-29 14:37:52 +0100

    [feat] - add route to get drug by id

--- api/routes/v1.js
+++ api/routes/v1.js
@@ -15,10 +15,21 @@ router.get('/pharmacies', function(req, res, next) {
 	 })
 });
 
-router.get('/drugs/:label', function(req, res, next) {
+router.get('/drugs/label/:label', function(req, res, next) {
 	var label = req.params.label
 	db.getConnection().then((conn) => {
 		conn.query('SELECT * from drugs where label like "%' + label.toUpperCase() + '%"', function (error, results, fields) {
+			if (error) throw error;
+			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+			conn.release();
+		});
+	 })
+});
+
+router.get('/drugs/id/:id', function(req, res, next) {
+	var id = req.params.id
+	db.getConnection().then((conn) => {
+		conn.query('SELECT * from drugs where id=' + id, function (error, results, fields) {
 			if (error) throw error;
 			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
 			conn.release();
