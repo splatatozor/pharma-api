ab5e505d4ac8daa548222a973cb78927dabfbd76
samuel Kabbaj <kabbaj.samuel@gmail.com>
2018-12-13 10:34:49 +0100

    [fix] - change ip to connect to database

--- api/database.js
+++ api/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const util = require('util')
 
 const pool  = mysql.createPool({
-	host     : '172.17.0.2',
+	host     : '163.172.167.185',
 	port	 : '3306',
 	user     : 'root',
 	password : 'soleil',
